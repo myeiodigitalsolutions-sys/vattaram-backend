37af145de1fe8fccba119a78c0fbbad9121583a9
myeiodigitalsolutions-sys <myeiodigitalsolutions@gmail.com>
2025-10-07 11:56:38 +0530

    Add files via upload

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,7 +3,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'vattaram-63357.appspot.com'
+  storageBucket: 'vattaram-63357.firebasestorage.app' // Use the correct bucket name
 });
 
 module.exports = admin;
